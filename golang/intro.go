4b2fd7ece577899f3e152d56fc576170a8478228
Slozzyondul <solomonondula@gmail.com>
2024-07-18 10:38:02 +0300

    calling my first go function

--- golang/intro.go
+++ golang/intro.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Println("Hello, World!")
+	solo()
+}
+
+func solo() {
+	fmt.Println("Solo, you need to learn Go in 2024")
 }
