4376723a24d0fc6d44fb507e93b3a8ff3ff91a4f
Slozzyondul <solomonondula@gmail.com>
2024-07-18 12:12:27 +0300

    restructuring my code to import my other code in other files

---
+++ golang/intro/introfile.go
@@ -0,0 +1,15 @@
+package intro
+
+import (
+	"fmt"
+	"time"
+)
+
+func Solo() {
+	fmt.Println("Solo, you need to learn Go in 2024")
+}
+
+// printing current time
+func Currentime() {
+	fmt.Println("current time is", time.Now())
+}
---
+++ golang/packages.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"golang/intro"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("My favorite number is", rand.Intn(10))
+	intro.Solo()
+	intro.Currentime()
+}
